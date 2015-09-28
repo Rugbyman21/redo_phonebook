8a96c4edf2d036088d8aece9540882c02e786719
Brandon & Trevor <student@epicodus.com>
2015-09-28 16:34:49 -0700

    add class and code for front end

--- lib/contact.rb
+++ lib/contact.rb
@@ -1,30 +1,116 @@
 class Contact
   attr_reader(:name, :job, :company)
 
+  @@contacts = []
+
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
     @job = attributes.fetch(:job)
     @company = attributes.fetch(:company)
+    @id = attributes.fetch(:id)
+  end
+
+  define_singleton_method(:all) do
+    @@contacts
+  end
+
+  define_singleton_method(:save) do
+    @@contacts.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@contacts = []
+  end
+
+  define_method(:id) do
+    @id.to_i
+  end
+
+  define_singleton_method(:find) do |id|
+    found_contact = nil
+    @@contacts.each() do |contact|
+      if contact.id().eql?(id)
+        found_contact = contact
+      end
+    end
+    found_contact
   end
 end
 
 class Address
-  attr_reader(:street, :city, :state, :zip, :number)
+  attr_reader(:street, :city, :state, :zip)
+
+  @@addresses = []
 
   define_method(:initialize) do |attributes|
     @street = attributes.fetch(:street)
     @city = attributes.fetch(:city)
     @state = attributes.fetch(:state)
     @zip = attributes.fetch(:zip)
-    @number = attributes.fetch(:number)
+    @id = attributes.fetch(:id)
+  end
+
+  define_singleton_method(:all) do
+    @@addresses
+  end
+
+  define_singleton_method(:save) do
+    @@addresses.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@addresses = []
+  end
+
+  define_method(:id) do
+    @id.to_i
+  end
+
+  define_singleton_method(:find) do |id|
+    found_address = nil
+    @@addresses.each() do |address|
+      if address.id().eql?(id)
+        found_address = address
+      end
+    end
+    found_address
   end
 end
 
 class Phone
   attr_reader(:home, :work)
 
+  @@phones = []
+
   define_method(:initialize) do |attributes|
     @home = attributes.fetch(:home)
     @work = attributes.fetch(:work)
+    @id = attributes.fetch(:id)
+  end
+
+  define_singleton_method(:all) do
+    @@phones
+  end
+
+  define_singleton_method(:save) do
+    @@phones.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@phones = []
+  end
+
+  define_method(:id) do
+    @id.to_i
+  end
+
+  define_singleton_method(:find) do |id|
+    found_phone = nil
+    @@phones.each() do |phone|
+      if phone.id().eql?(id)
+        found_phone = phone
+      end
+    end
+    found_phone
   end
 end
--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -8,22 +8,29 @@ describe(Contact) do
 
   describe('#name') do
     it('returns the contact name') do
-      test_contact = Contact.new({:name => "Joe Little", :job => "Mailman", :company => "US Postal Service"})
+      test_contact = Contact.new({:name => "Joe Little", :job => "Mailman", :company => "US Postal Service", :id => "1"})
       expect(test_contact.name()).to(eq("Joe Little"))
     end
   end
 
   describe('#job') do
     it('returns the contact job') do
-      test_contact = Contact.new({:name => "Joe Little", :job => "Mailman", :company => "US Postal Service"})
+      test_contact = Contact.new({:name => "Joe Little", :job => "Mailman", :company => "US Postal Service", :id => "1"})
       expect(test_contact.job()).to(eq("Mailman"))
     end
   end
 
   describe('#company') do
     it('returns the contact company') do
-      test_contact = Contact.new({:name => "Joe Little", :job => "Mailman", :company => "US Postal Service"})
+      test_contact = Contact.new({:name => "Joe Little", :job => "Mailman", :company => "US Postal Service", :id => "1"})
       expect(test_contact.company()).to(eq("US Postal Service"))
+    end
+  end
+
+  describe("#id") do
+    it("returns a contacts number with a id number") do
+      test_contact = Contact.new({:name => "Joe Little", :job => "Mailman", :company => "US Postal Service", :id => "1"})
+      expect(test_contact.id()).to(eq(1))
     end
   end
 end
@@ -32,36 +39,36 @@ describe(Address) do
 
   describe('#street') do
     it('returns the address street') do
-      test_address = Address.new({:street => "57 Barnes Rd.", :city => "Portland", :state => "Oregon", :zip => "97210", :number => "5031234567"})
+      test_address = Address.new({:street => "57 Barnes Rd.", :city => "Portland", :state => "Oregon", :zip => "97210", :number => "5031234567", :id => "1"})
       expect(test_address.street()).to(eq("57 Barnes Rd."))
     end
   end
 
   describe('#city') do
     it('returns the address city') do
-      test_address = Address.new({:street => "57 Barnes Rd.", :city => "Portland", :state => "Oregon", :zip => "97210", :number => "5031234567"})
+      test_address = Address.new({:street => "57 Barnes Rd.", :city => "Portland", :state => "Oregon", :zip => "97210", :number => "5031234567", :id => "1"})
       expect(test_address.city()).to(eq("Portland"))
     end
   end
 
   describe('#state') do
     it('returns the address state') do
-      test_address = Address.new({:street => "57 Barnes Rd.", :city => "Portland", :state => "Oregon", :zip => "97210", :number => "5031234567"})
+      test_address = Address.new({:street => "57 Barnes Rd.", :city => "Portland", :state => "Oregon", :zip => "97210", :number => "5031234567", :id => "1"})
       expect(test_address.state()).to(eq("Oregon"))
     end
   end
 
   describe('#zip') do
     it('returns the address zip') do
-      test_address = Address.new({:street => "57 Barnes Rd.", :city => "Portland", :state => "Oregon", :zip => "97210", :number => "5031234567"})
+      test_address = Address.new({:street => "57 Barnes Rd.", :city => "Portland", :state => "Oregon", :zip => "97210", :number => "5031234567", :id => "1"})
       expect(test_address.zip()).to(eq("97210"))
     end
   end
 
-  describe('#number') do
-    it('returns the phone number') do
-      test_address = Address.new({:street => "57 Barnes Rd.", :city => "Portland", :state => "Oregon", :zip => "97210", :number => "5031234567"})
-      expect(test_address.number()).to(eq("5031234567"))
+  describe("#id") do
+    it("returns a contacts number with a id number") do
+      test_address = Address.new({:street => "57 Barnes Rd.", :city => "Portland", :state => "Oregon", :zip => "97210", :number => "5031234567", :id => "1"})
+      expect(test_address.id()).to(eq(1))
     end
   end
 end
@@ -70,15 +77,22 @@ describe(Phone) do
 
   describe('#home') do
     it('returns the home phone number') do
-      test_phone = Phone.new({:home => "5038062572", :work => "5037899989"})
+      test_phone = Phone.new({:home => "5038062572", :work => "5037899989", :id => "1"})
       expect(test_phone.home()).to(eq("5038062572"))
     end
   end
 
   describe('#work') do
     it('returns the work phone number') do
-      test_phone = Phone.new({:home => "5038062572", :work => "5037899989"})
+      test_phone = Phone.new({:home => "5038062572", :work => "5037899989", :id => "1"})
       expect(test_phone.work()).to(eq("5037899989"))
+    end
+  end
+
+  describe("#id") do
+    it("returns a contacts number with a id number") do
+      test_phone = Phone.new({:home => "5038062572", :work => "5037899989", :id => "1"})
+      expect(test_phone.id()).to(eq(1))
     end
   end
 end
