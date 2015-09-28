e706bec41476d139b21e3fd648c5f6b01a6175f3
Brandon & Trevor <student@epicodus.com>
2015-09-28 15:43:58 -0700

    Added Phone Class

--- lib/contact.rb
+++ lib/contact.rb
@@ -9,10 +9,22 @@ class Contact
 end
 
 class Address
-  attr_reader(:street, :city)
+  attr_reader(:street, :city, :state, :zip, :number)
 
   define_method(:initialize) do |attributes|
     @street = attributes.fetch(:street)
     @city = attributes.fetch(:city)
+    @state = attributes.fetch(:state)
+    @zip = attributes.fetch(:zip)
+    @number = attributes.fetch(:number)
+  end
+end
+
+class Phone
+  attr_reader(:home, :work)
+
+  define_method(:initialize) do |attributes|
+    @home = attributes.fetch(:home)
+    @work = attributes.fetch(:work)
   end
 end
--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -32,15 +32,53 @@ describe(Address) do
 
   describe('#street') do
     it('returns the address street') do
-      test_address = Address.new({:street => "57 Barnes Rd.", :city => "Portland"})
+      test_address = Address.new({:street => "57 Barnes Rd.", :city => "Portland", :state => "Oregon", :zip => "97210", :number => "5031234567"})
       expect(test_address.street()).to(eq("57 Barnes Rd."))
     end
   end
 
   describe('#city') do
     it('returns the address city') do
-      test_address = Address.new({:street => "57 Barnes Rd.", :city => "Portland"})
+      test_address = Address.new({:street => "57 Barnes Rd.", :city => "Portland", :state => "Oregon", :zip => "97210", :number => "5031234567"})
       expect(test_address.city()).to(eq("Portland"))
+    end
+  end
+
+  describe('#state') do
+    it('returns the address state') do
+      test_address = Address.new({:street => "57 Barnes Rd.", :city => "Portland", :state => "Oregon", :zip => "97210", :number => "5031234567"})
+      expect(test_address.state()).to(eq("Oregon"))
+    end
+  end
+
+  describe('#zip') do
+    it('returns the address zip') do
+      test_address = Address.new({:street => "57 Barnes Rd.", :city => "Portland", :state => "Oregon", :zip => "97210", :number => "5031234567"})
+      expect(test_address.zip()).to(eq("97210"))
+    end
+  end
+
+  describe('#number') do
+    it('returns the phone number') do
+      test_address = Address.new({:street => "57 Barnes Rd.", :city => "Portland", :state => "Oregon", :zip => "97210", :number => "5031234567"})
+      expect(test_address.number()).to(eq("5031234567"))
+    end
+  end
+end
+
+describe(Phone) do
+
+  describe('#home') do
+    it('returns the home phone number') do
+      test_phone = Phone.new({:home => "5038062572", :work => "5037899989"})
+      expect(test_phone.home()).to(eq("5038062572"))
+    end
+  end
+
+  describe('#work') do
+    it('returns the work phone number') do
+      test_phone = Phone.new({:home => "5038062572", :work => "5037899989"})
+      expect(test_phone.work()).to(eq("5037899989"))
     end
   end
 end
