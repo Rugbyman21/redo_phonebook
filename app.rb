f62d2a519b66da733153086d5cd48c3424d01dae
Brandon & Trevor <student@epicodus.com>
2015-09-28 16:40:56 -0700

    bring up sinatra page in the browser

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
-# require('sinatra')
-# require('sinatra/reloader')
-# also_reload('lib/**/*.rb')
-# require('./lib/cd')
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/contact')
 # require('./lib/album')
-# require('pry')
-#
-# get('/') do
-#   erb(:index)
-# end
+require('pry')
+
+get('/') do
+  erb(:index)
+end
